b6e68ad938d6b25bc914c9abe9a2fb071186fb09
MateoSantobuono <83820800+MateoSantobuono@users.noreply.github.com>
2023-11-08 13:41:58 -0300

    refacture: Cambiar mensaje mostrado

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ void loop()
   int sensorValue = analogRead(sensorPin);
   float temperatureC = ((sensor * 5.0)/1024)/100;
   lcd.clear();
-  lcd.print("Temp =");
+  lcd.print("Temperatura: ");
   sensor = analogRead(A0); //leemos el valor del sensor del pin A0
 
   lcd.setCursor(0,1);
