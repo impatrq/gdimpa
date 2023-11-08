b1d50e0bbf1ba5d40fa49d795319b6ec06e2c10e
MateoSantobuono <83820800+MateoSantobuono@users.noreply.github.com>
2023-11-08 13:59:22 -0300

    feat: Agregar codigo de posicionamiento de cursor

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 LiquidCrystal_I2C lcd(0x3F,20,4); 
 
-const int sensorPin = A0;
+const int sensorPin = A0; 
 
 void setup() {
   lcd.begin(16, 2);  // Inicializa el LCD con 16 columnas y 2 filas
@@ -14,6 +14,7 @@ void loop()
   int sensorValue = analogRead(sensorPin);
   float temperatureC = ((sensor * 5.0)/1024)/100;
   lcd.clear();
+  lcd.setCursor(0, 0);
   lcd.print("Temperatura: ");
   lcd.setCursor(0,1);
   lcd.print(temperatureC);
