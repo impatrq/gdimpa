88801ff6e50d5dfa68b02d57b998d7a0c3b00d4d
MateoSantobuono <83820800+MateoSantobuono@users.noreply.github.com>
2023-11-08 13:37:21 -0300

    refactor: optimizar seteo de variable
    Esto para optimizar el codigo

--- main.c
+++ main.c
@@ -10,11 +10,11 @@ void setup() {
 void loop()
 {
   int sensorValue = analogRead(sensorPin);
-  float temperatura;
+  float temperatureC = ((sensor * 5000.0)/1023)/10;
   lcd.clear();
   lcd.print("Temp =");
   sensor = analogRead(A0); //leemos el valor del sensor del pin A0
-  temperatura = ((sensor * 5000.0)/1023)/10; //Formula para saber temp. con voltaje y el sensor
+
   lcd.setCursor(0,1);
   lcd.print(temperatura);
   delay (1000);
