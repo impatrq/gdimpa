9475ad80ba85c8026a482953ef10cf7023628b98
MateoSantobuono <83820800+MateoSantobuono@users.noreply.github.com>
2023-11-08 14:25:38 -0300

    fix: borrar códigos innecesario

--- main.c
+++ main.c
@@ -10,13 +10,9 @@ void setup() {
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(3,0);
-  lcd.print("Hello, world!");
   lcd.setCursor(2,1);
-  lcd.print("Ywrobot Arduino!");
-   lcd.setCursor(0,2);
-  lcd.print("Arduino LCM IIC 2004");
-   lcd.setCursor(2,3);
-  lcd.print("Power By Ec-yuan!");
+  lcd.setCursor(0,2);
+  lcd.setCursor(2,3);
 }
 
 void loop()
